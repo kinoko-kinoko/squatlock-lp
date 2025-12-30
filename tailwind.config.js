c1aa672a95ac94b4156f920deeda8325e5c7ee94
kohei <kino1989@gmail.com>
2025-12-30 10:46:15 +0900

    LPサイトの初期構築: Vite, React, Tailwind CSS環境設定およびコンテンツ実装

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'neon-green': '#ccff00',
+      },
+    },
+  },
+  plugins: [],
+}
